f3b0e1b0d5289262fcef8e262a80be1d8ffa14c7
bonath <vannbonath1@gmail.com>
2024-07-18 16:00:14 +0700

    Ohno

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,11 @@ module.exports = {
   apps: [
     {
       name: "product-service",
-      script: "./build/server.js",
+      script: "./server.js",
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production",
-      },
-      env_local: {
-        NODE_ENV: "development",
-      },
     },
   ],
 };
